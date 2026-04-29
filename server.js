e80b3ac7015810339f3e54a6a6ab34c1a413aecb
roma12345dj-code <roma12345dj@gmail.com>
2026-04-29 09:03:48 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    message: "Telegram Planner Backend работает"
+  });
+});
